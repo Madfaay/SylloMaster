f8c97d080a7fd1fef20bd30ace58bc9fd468d688
Madfaay <imadfaay123@gmail.com>
2024-11-25 23:48:28 +0100

    Tableau

---
+++ Doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.example.syllllogysme","l":"SyllogismeInterface"},{"m":"org.example.syllllogysme","l":"traitement"}];updateSearchResults();
